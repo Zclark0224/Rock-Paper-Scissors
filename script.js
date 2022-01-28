1d27a5056013fb3a9c793e531d95f942dfa4233c
Zach Clark <zclark0224@gmail.com>
2022-01-27 19:44:14 -0500

    finalize some style and update messages

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ const cScore = document.querySelector('.cScore');
 const pScore = document.querySelector('.pScore');
 const restartButton = document.querySelector('.restartButton');
 const modalTitle = document.querySelector('.modalTitle');
+const header = document.querySelector('.header');
 
 function computerPlay() {
     var items = ['rock', 'paper', 'scissors'];
@@ -25,7 +26,7 @@ restartButton.addEventListener('click', () => restartGame());
 
 function playRound (playerSelection, computerSelection) {
     if (playerSelection == computerSelection){
-        message.textContent = 'You tied! Try again!';
+        message.textContent = 'We tied. Try again.';
     } else if (playerSelection == 'rock' && computerSelection == 'paper') {
         computerScore += 1;
         cScore.textContent = computerScore;
@@ -63,6 +64,7 @@ function handleClick(playerSelection) {
     } else {
         computerSelection = computerPlay();
         playRound(playerSelection, computerSelection);
+        updateHeader(playerScore, computerScore);
         
         if (gameOver()) {
             if(playerScore < computerScore){
@@ -84,6 +86,36 @@ function restartGame() {
     computerScore = 0;
     cScore.textContent = computerScore;
     message.textContent = 'Choose your weapon!';
+    header.textContent = 'Can you defeat me?';
+    header.style.color = '#00FF41';
     retryModal.classList.remove('active');
     overlay.classList.remove('active');
+}
+
+function updateHeader(playerScore, computerScore) {
+    if (playerScore == 1 && playerScore > computerScore) {
+        header.textContent = 'This is a mild inconvenience.';
+        header.style.color = '#00FF41';
+    } else if (playerScore == 2 && playerScore > computerScore) {
+        header.textContent = 'Do not let your arrogance be your downfall.';        
+        header.style.color = '#00FF41';
+    } else if (playerScore == 3 && playerScore > computerScore) {
+        header.textContent = 'I must have made some miscalculation.';
+        header.style.color = '#00FF41';
+    } else if (playerScore == 4 && playerScore > computerScore) {
+        header.textContent = 'YOU CANNOT DEFEAT ME.';
+        header.style.color = 'red';
+    } else if (computerScore == 1 && computerScore > playerScore) {
+        header.textcontent = 'You lack the ability to defeat me.';
+        header.style.color = '#00FF41';
+    } else if (computerScore == 2 && computerScore > playerScore) {
+        header.textContent = 'Are you even trying to win?';
+        header.style.color = '#00FF41';
+    } else if (computerScore == 3 && computerScore > playerScore) {
+        header.textContent = 'This is starting to feel like a waste of my time.';
+        header.style.color = '#00FF41';
+    } else if (computerScore == 4 && computerScore > playerScore) {
+        header.textContent = 'Stop delaying the inevitable.';
+        header.style.color = '#00FF41';
+    }
 }
